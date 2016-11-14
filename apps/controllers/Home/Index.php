fcf34d88039162e5206802af9383e0a81b1e2962
huaerjie <286252682@qq.com>
2016-11-14 20:01:58 +0800

    1.修改api controller继承类

--- apps/controllers/Home/Index.php
+++ apps/controllers/Home/Index.php
@@ -8,20 +8,9 @@
 
 namespace controllers\Home;
 
-use service\TestService;
-use ZPHP\Cache\Factory;
-use ZPHP\Cache\ICache;
 use ZPHP\Controller\Apicontroller;
-use ZPHP\Controller\Controller;
-use ZPHP\Core\Config;
-use ZPHP\Core\Log;
-use ZPHP\Core\Db;
-use ZPHP\Coroutine\Http\HttpClientCoroutine;
-use ZPHP\Db\Mongo;
-use ZPHP\Manager\Redis;
-use ZPHP\Model\Model;
 
-class Index extends Controller{
+class Index extends Apicontroller{
     public function index(){
         return ['data'=>'hello zapi'];
     }
