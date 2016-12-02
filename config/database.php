6a90625b3fcb2008e884519556b17f563b586b75
huaerjie <286252682@qq.com>
2016-12-02 21:37:03 +0800

    1增加启动时连接池的初始化

--- config/database.php
+++ config/database.php
@@ -4,10 +4,11 @@
             'database'=>[
                     'master' => [
                         'host' => '127.0.0.1',
-                        'user' => 'jeekzx',
-                        'password' => '7f331f',
+                        'user' => 'keaixiaou',
+                        'password' => '135167',
                         'database' => 'jeekzx',
-                        'asyn_max_count' => 4,
+                        'asyn_max_count' => 10,
+                        'start_count' => 1,
                     ],
                 ]
 
