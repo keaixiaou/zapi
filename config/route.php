bc69f430cdd844925f650c53f4dd3cb4618fac5f
huaerjie <286252682@qq.com>
2017-01-05 13:21:07 +0800

    1.适配新版本

--- config/route.php
+++ config/route.php
@@ -9,28 +9,6 @@
 
 return [
     'route'=>[
-        'GET' => [
-            '/testmodel/{key}' => function($key){
-                return \ZPHP\Core\App::model('test')->test($key);
-            },
-            '/testindex' => function(){return 111;},
-        ],
-        'POST' => [
 
-            '/testinfo/{id}' => function($id){
-                return $id;
-            },
-        ],
-        'ANY' => [
-            '/' => 'Index\main',
-            '/user/{name}/no/{id}' => function($id, $name){
-                $data = yield \ZPHP\Core\App::model('test')->getUserDetail($id, $name);
-                return ['data'=>$data];
-            },
-
-            '/user/{id}' => function($id){
-                return \ZPHP\Core\App::model('test')->getUserById($id);
-            }
-        ],
     ],
 ];
