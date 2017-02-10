bf2968b52401ca941906aa5cd2d1f863870491e0
huaerjie <286252682@qq.com>
2017-02-10 17:02:00 +0800

    1.修改mysql配置文件

---
+++ config/mysql.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaoye
+ * Date: 2017/2/10
+ * Time: 下午4:51
+ */
+
+return [
+    'mysql'=>[
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'user' => 'test',
+        'password' => 'test',
+        'database' => 'test',
+        'asyn_max_count' => 0,
+        'start_count' => 0,
+    ],
+
+];
